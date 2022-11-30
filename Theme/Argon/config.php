842da74b7c07937d80e9dc208e76e6dfcdefe62d
Martindev <Delebecque.martin@gmail.com>
2022-11-30 23:23:58 +0100

    update thème argon norme

--- Theme/Argon/config.php
+++ Theme/Argon/config.php
@@ -13,6 +13,7 @@ return [
         return 'mt-2';
     })),
 
+    //'bootstrapv5' => true,
     'theme_primary' => \ClientX\setting("theme_primary"),
     'argon_theme' => \ClientX\setting("argon_theme", "none"),
     'argon_default' => \ClientX\setting("argon_default", "light"),
