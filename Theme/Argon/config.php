d3f50d98c5a3120be23be8ef408c50eb7d582d64
Martindev <Delebecque.martin@gmail.com>
2023-09-09 17:02:10 +0200

    Update config.php

--- Theme/Argon/config.php
+++ Theme/Argon/config.php
@@ -5,15 +5,9 @@ use function DI\get;
 
 return [
     'class' => \Themes\Argon\ArgonTheme::class,
-    \ClientX\Twig\FormExtension::class => \DI\autowire()->constructorParameter('classGroup', \DI\factory(function(\Psr\Container\ContainerInterface  $container){
-        $adminPrefix = $container->get('admin.prefix');
-        if (\ClientX\Helpers\Str::startsWith(\ClientX\request()->getUri()->getPath(), $adminPrefix)){
-            return 'form-group';
-        }
-        return 'mt-2';
-    })),
-
+    \ClientX\Twig\FormExtension::class => \DI\autowire()->constructorParameter('classGroup', 'mt-2'),
     //'bootstrapv5' => true,
+     'theme.disabledtextpagination' => true,
     'theme_primary' => \ClientX\setting("theme_primary"),
     'argon_theme' => \ClientX\setting("argon_theme", "none"),
     'argon_default' => \ClientX\setting("argon_default", "light"),
