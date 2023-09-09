9dcef981d5dc0d504fdfa507b0b63620b7071f9e
Martindev <Delebecque.martin@gmail.com>
2023-09-09 17:08:50 +0200

    Update config.php

--- Theme/Argon/config.php
+++ Theme/Argon/config.php
@@ -7,7 +7,10 @@ return [
     'class' => \Themes\Argon\ArgonTheme::class,
     \ClientX\Twig\FormExtension::class => \DI\autowire()->constructorParameter('classGroup', 'mt-2'),
     //'bootstrapv5' => true,
-     'theme.disabledtextpagination' => true,
+        'theme.pagination' => [
+        'prev_message' => '&laquo;',
+        'next_message' => '&raquo;',
+    ],
     'theme_primary' => \ClientX\setting("theme_primary"),
     'argon_theme' => \ClientX\setting("argon_theme", "none"),
     'argon_default' => \ClientX\setting("argon_default", "light"),
